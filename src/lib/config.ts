ebf79cdd425e8eed028b6c7a0343d073f73edf5d
cainiao524 <2862327335@qq.com>
2026-08-05 09:52:29 +0800

    版本号更新为 beta1.6.7-DEEPSEEK是个大baka

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 export const APP_CONFIG = {
-  version: "beta1.6.1-baka⑨",
+  version: "beta1.6.7-DEEPSEEK是个大baka",
   githubUrl: "https://github.com/cainiao524/qbittorrent-next-ui",
   name: "qBittorrent Next"
 }
