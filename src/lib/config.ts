b6c3e493d787d23dc6fee5798a5114042e1a2fc6
cainiao524 <2862327335@qq.com>
2026-08-06 10:04:50 +0800

    同步版本号 v1.6-beta-funky⑨：package.json 升 1.6.0，界面显示版本对齐

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 export const APP_CONFIG = {
-  version: "v1.3-funky⑨",
+  version: "v1.6-beta-funky⑨",
   githubUrl: "https://github.com/cainiao524/qbittorrent-next-ui",
   name: "qBittorrent Next"
 }
