50bfe1fc9798a99f820e9a806f5b90ed5c349999
cainiao524 <2862327335@qq.com>
2026-08-10 20:07:41 +0800

    发布：v1.17.2-funky9

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 export const APP_CONFIG = {
-  version: "v1.17.1-funky⑨",
+  version: "v1.17.2-funky⑨",
   githubUrl: "https://github.com/cainiao524/qbittorrent-next-ui",
   name: "qBittorrent Next"
 }
