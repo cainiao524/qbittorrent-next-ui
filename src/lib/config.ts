5bec469150bd642a56b50abe585e919e16d93797
cainiao524 <57671894+cainiao524@users.noreply.github.com>
2026-08-12 00:57:32 +0800

    feat: 统一进度显示并默认展开文件树 (#31)

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 export const APP_CONFIG = {
-  version: "v1.18.0-funky⑨",
+  version: "v1.18.1-funky⑨",
   githubUrl: "https://github.com/cainiao524/qbittorrent-next-ui",
   name: "qBittorrent Next"
 }
