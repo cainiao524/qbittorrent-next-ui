b6866787c9d9d10887342e19b5b46c26ba60cf36
cainiao524 <2862327335@qq.com>
2026-08-23 01:46:05 +0800

    发布 v1.18.6-funky9：优化主页面滚动与侧栏动画

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 export const APP_CONFIG = {
-  version: "v1.18.5-funky⑨",
+  version: "v1.18.6-funky⑨",
   githubUrl: "https://github.com/cainiao524/qbittorrent-next-ui",
   name: "qBittorrent Next"
 }
